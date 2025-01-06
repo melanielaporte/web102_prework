4d7aa1c0294aca55c01663753d404ab025f7a680
melanielaporte <melaniemlaporte@protonmail.com>
2025-01-05 20:05:22 -0600

    Updated js.

--- index.js
+++ index.js
@@ -42,20 +42,29 @@ function addGamesToPage(games) {
         const gameCard = document.createElement("div");
 
         // add the class game-card to the list
-
+        gameCard.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        gameCard.innerHTML = 
+            <img class="game-img" src="${game.image}" alt="${game.name}">
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+            <p>Raised: $${game.pledged.toLocaleString()}</p>
+            <p>Goal: $${game.goal.toLocaleString()}</p>
+    ;
 
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    }
 }
 
 // call the function we just defined using the correct variable
+addGamesToPage(GAMES_JSON);
 // later, we'll call this function using a different list of games
+
 
 
 /*************************************************************************************
